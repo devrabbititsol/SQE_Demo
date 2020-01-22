a4bd18f2174a6bbb24dbd59cb0a47565450af5cb
root <santhosh.annam@devrabbit.com>
2020-01-22 10:22:07 +0530

    hello

---
+++ src/test/java/api/sqe_demo/api_test_module/apipageclasses/API_DELETEDataset.java
@@ -0,0 +1,70 @@
+package api.sqe_demo.api_test_module.apipageclasses; 
+
+public class API_DELETEDataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "sqe_demo";
+
+ public static String primaryInfo  = "{\"module_description\":\"desc\","
+ + "\"docker_files_directory_url\":\"https://smartqe.io:443/pdownload/DockerFiles\","
+ + "\"mobile_platform\":null,"
+ + "\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\","
+ + "\"repository_commit_message\":\"hello\","
+ + "\"browser_type\":null,"
+ + "\"client_timezone_id\":\"Asia/Calcutta\","
+ + "\"docker_files_directory\":\"/opt/tomcat9/webapps/pdownload/DockerFiles\","
+ + "\"project_url\":\"https://reqres.in/\","
+ + "\"jenkins_username\":\"devrabbit\","
+ + "\"testcase_id\":315,"
+ + "\"project_name\":\"SQE_Demo\","
+ + "\"testset_name\":null,"
+ + "\"is_web\":false,"
+ + "\"jenkins_password\":\"rabbit\","
+ + "\"testcase_overwrite\":false,"
+ + "\"docker_file_name\":\"DockerFile_7_SE_TC_315\","
+ + "\"project_id\":343,"
+ + "\"framework_path\":\"/home/devrabbit/SmartQE/SmartQE-Users/7/Selenium/SQE_Demo\","
+ + "\"sub_module_id\":0,"
+ + "\"repository_url\":\"1nux/Oa7/Z1uRcUZ9tUGDUYcN2hpisj6nYMmawM21BZ6IvjToxm6kPKWQr6dVPecA1xC1klvgxuYRi2lz2aXsw5G4AlecA3jAwhNFUn3XaFfTHT0pgtK/lPIw0BCvnDT\","
+ + "\"sub_module_description\":null,"
+ + "\"testcase_name\":\"TC_CRUD_API\","
+ + "\"executed_timestamp\":1579668722392,"
+ + "\"is_execute\":false,"
+ + "\"file_name\":null,"
+ + "\"aes_secret_key\":\"_SmartQE_SmartQE\","
+ + "\"testset_id\":0,"
+ + "\"jenkins_token\":\"114de69b453efd3524fce8756b1ed0e69c\","
+ + "\"is_generate\":false,"
+ + "\"jenkins_url\":\"http://192.168.1.141:8080/\","
+ + "\"project_description\":\"desc\","
+ + "\"module_id\":630,"
+ + "\"user_id\":7,"
+ + "\"bundle_id\":null,"
+ + "\"sub_module_name\":null,"
+ + "\"device_os_version\":null,"
+ + "\"execution_environment\":\"jenkins\","
+ + "\"module_name\":\"API_test_module\","
+ + "\"executed_user_id\":7,"
+ + "\"desktop_automation\":false,"
+ + "\"input_command\":null}";
+
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "TC_CRUD_API-Default";
+
+public static final String datasetResources1 = "api/users/2";
+public static final int requestType1 = 4;
+public static final int bodyType1 = 3;
+
+ public static String urlParams1  = "[]";
+
+ public static String headers1  = "[]";
+
+ public static String body1  = "{\"raw_text\":null,"
+ + "\"raw_id\":0,"
+ + "\"dataset_id\":0,"
+ + "\"raw_type_id\":0}";
+
+}
